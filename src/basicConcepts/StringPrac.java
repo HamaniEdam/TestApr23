4ccf94f9e9cef73d9694f41ed8a2c4b59fda846a
HamaniEdam <132179129+HamaniEdam@users.noreply.github.com>
2023-05-23 20:41:44 +0530

    Delete src/basicConcepts directory

--- src/basicConcepts/StringPrac.java
+++
@@ -1,8 +0,0 @@
-package basicConcepts;
-
-public class StringPrac {
-	public static void main(String[] args) {
-		System.out.println("This is main method..of branch A");
-		System.out.println("Creating conflict scenrio from A Branch");
-	}
-}
