e7ec200a10a2e0a92942923000a8b4a377f5e7ac
Hamani Edam <ammuedam@gmail.com>
2023-05-23 08:09:42 +0530

    changes in stringprac file

--- src/basicConcepts/StringPrac.java
+++ src/basicConcepts/StringPrac.java
@@ -2,6 +2,7 @@ package basicConcepts;
 
 public class StringPrac {
 	public static void main(String[] args) {
-		System.out.println("This is main method");
+		System.out.println("This is main method..of branch A");
+		System.out.println("Creating conflict scenrio from A Branch");
 	}
 }
